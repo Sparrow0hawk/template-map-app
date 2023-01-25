d52cb7a04f626a0303aeadce556a4690b1cf2f79
Sparrow0hawk <alexjcoleman@gmail.com>
2023-01-25 20:36:47 +0000

    adding sveltestrap layout format

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,20 @@
 <script>
+	import { Styles } from 'sveltestrap';
 	import Map from './Map.svelte';
+	import Layout from './Layout.svelte';
 </script>
+
+<Styles />
 
 <svelte:head>
 	<title>Template maplibre-gl</title>
 </svelte:head>
+<Layout>
+	<div slot="sidebar">
+		<h1>A template svelte maplibre-gl site</h1>
+	</div>
 
-<h1>A template svelte maplibre-gl site</h1>
-
-<Map />
+	<div slot="map">
+		<Map />
+	</div>
+</Layout>
---
+++ src/routes/Layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { Col, Container, Row } from 'sveltestrap';
+</script>
+
+<Container fluid>
+	<Row>
+		<Col xs="4"><slot name="sidebar" /></Col>
+		<Col xs="8"><slot name="map" /></Col>
+	</Row>
+</Container>
