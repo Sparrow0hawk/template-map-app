20028eac5a1a4dfecedb075191b5c93e9d164e2b
Sparrow0hawk <alexjcoleman@gmail.com>
2023-01-25 15:13:22 +0000

    changes to enable deployment on gh-pages
    - create a deploy option that adds .nojekyll
    - specify base URL in svelte config
    - updated workflow to use deploy

--- svelte.config.js
+++ svelte.config.js
@@ -10,6 +10,9 @@ export default {
 			fallback: null,
 			precompress: false,
 			strict: true
-		})
+		}),
+		paths: {
+			base: process.env.NODE_ENV === "production" ? "/template-map-app" : ""
+		}
 	}
 };
